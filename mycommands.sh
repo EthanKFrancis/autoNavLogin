d76035608848f3963520605a36185c0f2148d9ee
nemesis <basix317@gmail.com>
2020-02-20 10:15:14 -0500

    code failed to run. currently debugging

--- mycommands.sh
+++ mycommands.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 #path env to python
 
-kill $(ps aux | grep Chrome)
+kill (ps aux | grep "Chrome")
 #kill any instances of webbrowser
 
-python3 /home/nemesis/Desktop/code/test/ '''.py''' /home/nemesis/Desktop/code/test/'''.txt'''
+python3 /home/nemesis/Desktop/code/test/login.py /home/nemesis/Desktop/code/test/websites.txt Chrome
 #direct path to py and txt files
 exit 0
 #terminal will close after running instructions
