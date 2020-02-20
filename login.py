7b4e5ae59305a52258b25631ae350111ead425ff
nemesis <basix317@gmail.com>
2020-02-20 15:09:16 -0500

    made some changes again. for the var browser, I changed the vaule from chrome to google-chrome and that worked when running python3 ./login.py in bash

--- login.py
+++ login.py
@@ -1,19 +1,12 @@
 import webbrowser
 import sys
-import os
-import datetime
-from datetime import time
-
-urL="http://google.com"
-chrome_path="/var/lib/alternatives/google-chrome"
-webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path))
-webbrowser.get('chrome %s').open_new_tab(urL)
+import time
 
 def main():
   print('Opening Favorite Sites')
 
   sites = "websites.txt"
-  browser = "chrome"
+  browser = "google-chrome"
 
   if len(sys.argv) >= 2:
     sites = sys.argv[1].lower()
@@ -21,16 +14,14 @@ def main():
   if len(sys.argv) >= 3:
     browser = sys.argv[2].lower()
 
-  global webbrowser 
-  webbrowser = webbrowser.get(browser)
+  wbbrowser = webbrowser.get(browser)
 
   with open(sites) as fobj:
     try:
       for num, url in enumerate(fobj):
-        webbrowser.open_new_tab(url.strip())
+        wbbrowser.open_new_tab(url.strip())
         time.sleep(1)
     except Exception as e:
       print(e)
 
-if __name__ == '__main__': 
-  main()
+if __name__ == '__main__': main()
