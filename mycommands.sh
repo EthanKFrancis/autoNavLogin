e75b076a8067d3ff1d59eacd24217c7e2334fc61
nemesis <basix317@gmail.com>
2020-02-20 09:29:57 -0500

    added code to .sh; included env, kill instance of already running app, full path to py and txt, and exit terminal instance when completed task

---
+++ mycommands.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#path env to python
+
+kill $(ps aux | grep Chrome)
+#kill any instances of webbrowser
+
+python3 /home/nemesis/Desktop/code/test/ '''.py''' /home/nemesis/Desktop/code/test/'''.txt'''
+#direct path to py and txt files
+exit 0
+#terminal will close after running instructions
